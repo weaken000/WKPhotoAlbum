16a13e76073d666ac7627a19c6bfb8690d8e3ada
weikun <845188093@qq.com>
2018-12-11 17:48:02 +0800

    清空文件

---
+++ WKPhotoAlbum/WKEmpty.h
@@ -0,0 +1,17 @@
+//
+//  WKEmpty.h
+//  WKPhotoAlbumSample
+//
+//  Created by mac on 2018/12/11.
+//  Copyright © 2018 weikun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKEmpty : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WKPhotoAlbum/WKEmpty.m
@@ -0,0 +1,13 @@
+//
+//  WKEmpty.m
+//  WKPhotoAlbumSample
+//
+//  Created by mac on 2018/12/11.
+//  Copyright © 2018 weikun. All rights reserved.
+//
+
+#import "WKEmpty.h"
+
+@implementation WKEmpty
+
+@end
